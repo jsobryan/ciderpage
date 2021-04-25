3d735af0637e7ca1882eff2690888fe08b0f6f2f
jsobryan <jsobryan@gmail.com>
2021-04-24 22:21:48 -0500

    beta

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
-const panels = document.querySelectorAll('.panel')
+const navbar = document.getElementById('navbar');
+let scrolled = false;
 
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    });
-});
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    });
+window.onscroll = function () {
+    if(window.pageYOffset > 100) {
+        navbar.classList.remove('top');
+        if(!scrolled) {
+            navbar.style.transform = 'translateY(-70px)';
+        }
+        setTimeout(function() {
+            navbar.style.transform = 'translateY(0)';
+            scrolled = true;
+        }, 200)
+    } else {
+        navbar.classList.add('top');
+        scrolled = false;
+    }
 }
 
+//Smooth scrolling
+$('#navbar a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
 
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100,
+            },
+            800
+        );
+    }
+});
